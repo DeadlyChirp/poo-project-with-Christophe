3147727ddeae48cc6ec0cd9e43925cb879e0916a
tangt <thanh-long.tang@etu.u-paris.fr>
2022-12-22 17:45:57 +0100

    Initial commit

--- Code/src/model/Domino.java
+++ Code/src/model/Domino.java
@@ -1,4 +1,27 @@
 package model;
 
-public class Domino {
-}
+    public abstract class Domino {
+
+        int x;
+        int y;
+        int[] Nord,Sud,Est,Ouest;
+
+        public Domino(int[] Nord, int[] Sud, int[] Est, int[] Ouest) {
+            if(Nord.length == 3 && Sud.length == 3 && Est.length == 3 && Ouest.length == 3){
+                this.Nord = Nord;
+                this.Sud = Sud;
+                this.Est = Est;
+                this.Ouest = Ouest;
+            }
+        }
+
+        public void rotateGauche(){
+            int[] temp = Nord;
+            Nord = Ouest;
+            Ouest = Sud;
+            Sud = Est;
+            Est = temp;
+        }
+
+    }
+
